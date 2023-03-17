f9c9921e9e78e1c36ec06c0d7154f0e23d909ace
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-17 19:34:58 -0300

    feat: este commit remove o component next/head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>App</h1>
-      </main>
-    </>
+    <main>
+      <h1>App</h1>
+    </main>
   );
 }
