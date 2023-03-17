8438cb895ef0fcac392e80d496231ac8989ab5f5
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-17 19:34:31 -0300

    feat: este commit adiciona os icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,15 +12,14 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="shortcut icon"
-          href="/img/icon-512.png"
+          href="/img/icon/icon-512.png"
           type="image/x-icon"
         />
         <link
           rel="apple-touch-icon"
-          href="/img/icon-512.png"
+          href="/img/icon/icon-512.png"
           type="image/x-icon"
         />
-        <link rel="manifest" href="/manifest.json" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
