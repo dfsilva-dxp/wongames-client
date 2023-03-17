d564058c9f4b11099b830162b428ded07eac0d4c
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-17 19:19:19 -0300

    feat: este commit define os estilos globais e tema da aplicação

---
+++ src/@types/styled-component.d.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styles";
+
+type Theme = typeof DefaultTheme;
+
+declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,29 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+
+import { DefaultTheme, GlobalStyles } from "styles";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <Head>
+        <title>Wongames</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="shortcut icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
