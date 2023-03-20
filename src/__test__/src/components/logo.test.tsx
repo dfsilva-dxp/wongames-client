61a11b0c73b90979c91c01a1d1f734eb25d19f84
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-20 19:18:56 -0300

    feat: este commit adiciona os testes do componente Logo

---
+++ src/__test__/src/components/logo.test.tsx
@@ -0,0 +1,56 @@
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helpers";
+
+import Logo from "components/Logo";
+
+describe("<Logo />", () => {
+  it("should render the logo with id passed", () => {
+    const { container } = renderWithTheme(<Logo id="logo" />);
+
+    expect(
+      container.querySelector("#linear_gradient_logo")
+    ).toBeInTheDocument();
+  });
+
+  it("should render a white label by default", () => {
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#FAFAFA"
+    });
+  });
+
+  it("should render a black label when color is passed", () => {
+    renderWithTheme(<Logo color="black" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#030517"
+    });
+  });
+
+  it("should render a normmal logo when size is default", () => {
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "11rem"
+    });
+  });
+
+  it("should render a bigger logo", () => {
+    renderWithTheme(<Logo size="large" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "20rem"
+    });
+  });
+
+  it("should render a bigger logo without text if hideOnMobile", () => {
+    renderWithTheme(<Logo hideOnMobile />);
+
+    waitFor(() => {
+      expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+        "width",
+        "5.8rem",
+        {
+          media: "(max-width: 768px)"
+        }
+      );
+    });
+  });
+});
