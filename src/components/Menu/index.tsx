6c1610016babcba43722a22342622710435f55cd
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-22 12:19:10 -0300

    feat: este commit adiciona o jsx e styles do componente menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import Link from "next/link";
+import { Menu2 as MenuIcon } from "@styled-icons/remix-line/Menu2";
+import { Search as SearchIcon } from "@styled-icons/material-outlined/Search";
+import { Close as CloseIcon } from "@styled-icons/material-outlined/Close";
+
+import Button from "components/Button";
+import CartDropdown from "components/CartDropdown";
+import CartIcon from "components/CartIcon";
+import Logo from "components/Logo";
+import MediaMatch from "components/MediaMatch";
+import UserDropdown from "components/UserDropdown";
+
+import * as S from "./styles";
+
+import { MenuProps } from "./menu";
+
+const Menu = ({ username }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <S.Wrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
+      <S.LogoWrapper>
+        <Link href="/" passHref>
+          <Logo hideOnMobile aria-label="Won Games" />
+        </Link>
+      </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
+
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
+        </S.MenuNav>
+      </MediaMatch>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <MediaMatch greaterThan="medium">
+            <CartDropdown />
+          </MediaMatch>
+
+          <MediaMatch lessThan="medium">
+            <Link href="/cart" passHref>
+              <CartIcon />
+            </Link>
+          </MediaMatch>
+        </S.IconWrapper>
+
+        <MediaMatch greaterThan="medium">
+          {!username ? (
+            <Link href="/sign-in" passHref>
+              <Button>Sign in</Button>
+            </Link>
+          ) : (
+            <UserDropdown username={username} />
+          )}
+        </MediaMatch>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <S.MenuNav>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
+
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
+
+          {!!username && (
+            <>
+              <Link href="/profile/me" passHref>
+                <S.MenuLink>My profile</S.MenuLink>
+              </Link>
+              <Link href="/profile/wishlist" passHref>
+                <S.MenuLink>Wishlist</S.MenuLink>
+              </Link>
+            </>
+          )}
+        </S.MenuNav>
+
+        {!username && (
+          <S.RegisterBox>
+            <Link href="/sign-in" passHref>
+              <Button fullWidth size="large">
+                Log in nows
+              </Button>
+            </Link>
+
+            <span>or</span>
+            <Link href="/sign-up" passHref>
+              <S.CreateAccount title="Sign Up">Sign Up</S.CreateAccount>
+            </Link>
+          </S.RegisterBox>
+        )}
+      </S.MenuFull>
+    </S.Wrapper>
+  );
+};
+
+export default Menu;
