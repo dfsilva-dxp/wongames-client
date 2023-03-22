b4d47279bcc0be34a8e989bea61c2b93c59bf8de
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-22 12:22:41 -0300

    test: este commit adiciona os testes do componente dropdown

---
+++ src/__test__/components/dropdown.test.tsx
@@ -0,0 +1,37 @@
+import { act, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { renderWithTheme } from "utils/tests/helpers";
+
+import Dropdown from "components/Dropdown";
+
+describe("<Dropdown />", () => {
+  beforeEach(() => {
+    const title = <h1 aria-label="dropdown">Click here</h1>;
+
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>content</span>
+      </Dropdown>
+    );
+  });
+
+  it("should render title", () => {
+    expect(screen.getByLabelText(/dropdown/)).toBeInTheDocument();
+  });
+
+  it("should handle open/close dropdown", () => {
+    const content = screen.getByText(/content/).parentElement!;
+
+    expect(content).toHaveStyle({ opacity: 0 });
+    expect(content.getAttribute("aria-hidden")).toBe("true");
+
+    act(() => {
+      userEvent.click(screen.getByLabelText(/dropdown/));
+    });
+
+    waitFor(() => {
+      expect(content).toHaveStyle({ opacity: 1 });
+      expect(content.getAttribute("aria-hidden")).toBe("false");
+    });
+  });
+});
