7d960c5d242389f5215b4b5131905951db6e146b
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-22 12:22:09 -0300

    test: este commit adiciona os testes do componente cart list

---
+++ src/__test__/components/cartList.test.tsx
@@ -0,0 +1,32 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helpers";
+
+import CartList from "components/CartList";
+
+import items from "mocks/cartList";
+
+describe("<CartList />", () => {
+  it("should render the cart list", () => {
+    const { container } = renderWithTheme(
+      <CartList items={items} total="R$ 330,00" />
+    );
+
+    expect(screen.getAllByRole("heading")).toHaveLength(2);
+    expect(screen.getByText("R$ 330,00")).toHaveStyle({ color: "#F231A5" });
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should render the button", () => {
+    renderWithTheme(<CartList items={items} total="R$ 330,00" hasButton />);
+
+    expect(screen.getByText(/Buy it now/i)).toBeInTheDocument();
+  });
+
+  it("should render empty if there are no games", () => {
+    renderWithTheme(<CartList />);
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument();
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument();
+  });
+});
