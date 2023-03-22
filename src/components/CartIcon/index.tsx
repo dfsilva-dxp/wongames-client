dd8feb19ad088ca9ef901030c79a287505d3d00b
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-22 12:16:04 -0300

    feat: este commit adiciona o jsx e styles do componente cart icon

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,16 @@
+import { ShoppingCart } from "@styled-icons/material-outlined/ShoppingCart";
+
+import * as S from "./styles";
+
+import { CartIconProps } from "./cartIcon";
+
+const CartIcon = ({ quantity = 0 }: CartIconProps) => {
+  return (
+    <S.Wrapper>
+      {quantity > 0 && <S.Badge aria-label="Cart Items">{quantity}</S.Badge>}
+      <ShoppingCart aria-label="Shopping Cart" />
+    </S.Wrapper>
+  );
+};
+
+export default CartIcon;
