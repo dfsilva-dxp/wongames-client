c1fe53350726c8eb87bd77e1b949f588275d69dd
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-22 12:31:00 -0300

    feat: este commit adiciona um mock para cart list

---
+++ src/mocks/cartList.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    img: "http://localhost:1337/uploads/Image_9620999b16.png",
+    title: "Red Dead Redemption 2",
+    price: 215.0
+  },
+  {
+    img: "http://localhost:1337/uploads/Image_9620999b16.png",
+    title: "Borderlands 3",
+    price: 215.0
+  }
+];
