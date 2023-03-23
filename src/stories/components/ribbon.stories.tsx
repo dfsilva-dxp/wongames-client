09fe78ca463b8f8b3d28d4959d3970f0539b3613
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-23 19:12:24 -0300

    feat: este commit adiciona as stories do componente ribbon

---
+++ src/stories/components/ribbon.stories.tsx
@@ -0,0 +1,101 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Ribbon from "components/Ribbon";
+
+const meta: Meta<typeof Ribbon> = {
+  title: "Components/Ribbon",
+  component: Ribbon,
+  tags: ["autodocs"],
+  argTypes: {
+    color: {
+      options: ["primary", "secondary"],
+      control: { type: "select" }
+    },
+    size: {
+      options: ["small", "normal", "large"],
+      control: { type: "select" }
+    }
+  },
+  args: {
+    children: "Best Seller",
+    color: "primary",
+    size: "normal"
+  },
+  parameters: {
+    layout: "centered",
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof Ribbon>;
+
+export const Small: Story = {
+  render: () => (
+    <div
+      style={{
+        width: "40rem",
+        height: "25rem",
+        position: "relative",
+        backgroundColor: "#fff"
+      }}
+    >
+      <Ribbon color="primary" size="small">
+        Best Seller
+      </Ribbon>
+    </div>
+  )
+};
+
+export const Normal: Story = {
+  render: () => (
+    <div
+      style={{
+        width: "40rem",
+        height: "25rem",
+        position: "relative",
+        backgroundColor: "#fff"
+      }}
+    >
+      <Ribbon color="primary" size="normal">
+        Best Seller
+      </Ribbon>
+    </div>
+  )
+};
+
+export const Large: Story = {
+  render: () => (
+    <div
+      style={{
+        width: "40rem",
+        height: "25rem",
+        position: "relative",
+        backgroundColor: "#fff"
+      }}
+    >
+      <Ribbon color="primary" size="large">
+        Best Seller
+      </Ribbon>
+    </div>
+  )
+};
+
+export const WithSecondaryColor: Story = {
+  render: () => (
+    <div
+      style={{
+        width: "40rem",
+        height: "25rem",
+        position: "relative",
+        backgroundColor: "#fff"
+      }}
+    >
+      <Ribbon color="secondary" size="large">
+        Best Seller
+      </Ribbon>
+    </div>
+  )
+};
