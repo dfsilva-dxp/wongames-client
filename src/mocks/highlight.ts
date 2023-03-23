215ea8de12eae59be445f2f1f412141e29326c13
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-23 19:13:02 -0300

    feat: este commit adiciona os mocks para o componente highlight

---
+++ src/mocks/highlight.ts
@@ -0,0 +1,8 @@
+export default {
+  title: "Read Dead it’s back",
+  subtitle: "Come see John’s new adventures",
+  backgroundImage: "http://localhost:1337/uploads/Background_1c16930d95.png",
+  floatImage: "http://localhost:1337/uploads/Image_0ddb2f89e7.png",
+  buttonLabel: "Buy now",
+  buttonLink: "/rdr2"
+};
