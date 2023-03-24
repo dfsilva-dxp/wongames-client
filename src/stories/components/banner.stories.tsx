8b4de2802acfe09271322c10008e6cd219c734b7
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-24 15:35:49 -0300

    refactor: este commit irá refatorar a doc das stories do componente banner

--- src/stories/components/banner.stories.tsx
+++ src/stories/components/banner.stories.tsx
@@ -7,13 +7,49 @@ const meta: Meta<typeof Banner> = {
   component: Banner,
   tags: ["autodocs"],
   argTypes: {
+    img: {
+      description: "Recebe a url da imagem que será renderizada no banner"
+    },
+    title: {
+      description: "Recebe o nome do jogo em destaque no banner"
+    },
+    subtitle: {
+      description:
+        "Define um subtítulo de descrição do game em destaque no banner"
+    },
+    buttonLabel: {
+      description: "Define o conteúdo do button"
+    },
+    buttonLink: {
+      description: "Define o link de redirect do button"
+    },
     ribbonSize: {
-      options: ["small", "normal", "large"],
-      control: { type: "select" }
+      description: "Opções de tamanho do ribbon.",
+      options: ["small", "medium", "large"],
+      control: { type: "select" },
+      table: {
+        defaultValue: {
+          summary: "normal"
+        },
+        type: {
+          summary: "Tamanhos.",
+          detail: "small, medium, large"
+        }
+      }
     },
     ribbonColor: {
+      description: "Opções de cores do ribbon.",
       options: ["primary", "secondary"],
-      control: { type: "select" }
+      control: { type: "select" },
+      table: {
+        defaultValue: {
+          summary: "primary"
+        },
+        type: {
+          summary: "Cores.",
+          detail: "primary, secondary"
+        }
+      }
     }
   },
   args: {
