0d479d37bfcca8843457e3e8a5252151cacc856c
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-27 18:38:57 -0300

    feat: este commit adiciona os mocks do banner slider

---
+++ src/mocks/bannerSlider.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    img: "http://localhost:1337/uploads/banner_de_venda_plana_com_foto_23_2149026968_193c6c10c4.avif",
+    title: "Defy death 1",
+    subtitle: "<p>Play the new <strong>CrashLands</strong> season",
+    buttonLabel: "Buy now",
+    buttonLink: "/games/defy-death",
+    ribbon: "Bestselling"
+  },
+  {
+    img: "http://localhost:1337/uploads/Image_9620999b16.png",
+    title: "Defy death 2",
+    subtitle: "<p>Play the new <strong>CrashLands</strong> season",
+    buttonLabel: "Buy now",
+    buttonLink: "/games/defy-death"
+  }
+];
