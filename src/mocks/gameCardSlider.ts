7e1885f588b827a05b718900dec2c4d6a88f8c6e
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-27 18:51:20 -0300

    feat: este commit adiciona os mocks para o componente game card slider

---
+++ src/mocks/gameCardSlider.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: "Population Zero",
+    slug: "population-zero",
+    developer: "Rockstar Games",
+    img: "http://localhost:1337/uploads/image_1_35be7f9bd3.png",
+    price: 215.0,
+    promotionalPrice: 115.0
+  },
+  {
+    title: "Population Zero",
+    slug: "population-zero",
+    developer: "Rockstar Games",
+    img: "http://localhost:1337/uploads/image_1_35be7f9bd3.png",
+    price: 215.0,
+    promotionalPrice: 115.0
+  },
+  {
+    title: "Population Zero",
+    slug: "population-zero",
+    developer: "Rockstar Games",
+    img: "http://localhost:1337/uploads/image_1_35be7f9bd3.png",
+    price: 215.0,
+    promotionalPrice: 115.0
+  },
+  {
+    title: "Population Zero",
+    slug: "population-zero",
+    developer: "Rockstar Games",
+    img: "http://localhost:1337/uploads/image_1_35be7f9bd3.png",
+    price: 215.0,
+    promotionalPrice: 115.0
+  },
+  {
+    title: "Population Zero",
+    slug: "population-zero",
+    developer: "Rockstar Games",
+    img: "http://localhost:1337/uploads/image_1_35be7f9bd3.png",
+    price: 215.0,
+    promotionalPrice: 115.0
+  }
+];
